082c3c9b6bde2852cf32946b90e5b6a2f3477af3
taimurtufail <tatufa@ttu.ee>
2017-11-20 02:35:11 +0200

    Update feed.js

--- routes/feed.js
+++ routes/feed.js
@@ -15,23 +15,22 @@ router.get('/:channel_name', function(req, res, next) {
   				var feed = new rss({
   					title:"#" + channel.name,
   					description:"The links that have been posted to the #"+channel.name +" on Slack",
-  					site_url: 'https://github.com/gozman/slack-rss',
+  					site_url: 'https://www.linnworks.com',
   					ttl: '30',
   				});
 
   				slack.api('channels.history', {'channel':channel.id,'count':process.env.HISTORY_LENGTH} ,function(err, response){
 			  		for(var i = 0; i < response.messages.length; i++) {
-			  			if(response.messages[i].attachments && response.messages[i].subtype != "bot_message") {  				
-				  			for(var j = 0; j < response.messages[i].attachments.length; j++) {
-				  				if(response.messages[i].attachments[j].title) {
-				  					var link = response.messages[i].attachments[j];
+			  			if(response.messages[i].text) {  				
+				  			for(var j = 0; j < response.messages[i].text.length;) {
+				  				if(response.messages[i].text) {
+				  					var link = response.messages[i].text;
 
 				  					var t = new Date(response.messages[i].ts * 1000);
 
 				  					feed.item({
-				  						title: link.title,
-				  						description: link.text,
-				  						url: link.title_link,
+                      title: "Important Updates"
+				  						description: link,
 				  						date: t
 				  					});
 				  				}
